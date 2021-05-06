71008fdcb93a5fa514347c05db77769ba0a91bc2
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-05-06 18:32:30 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState([]);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apiKey}`;
+
+  const fetchSearchCountry = async (e) => {
+    e.preventDefault();
+    const response = await fetch(url);
+    const weather = await response.json();
+    setWeather(weather);
+    console.log(weather);
+  };
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form className="App" onSubmit={fetchSearchCountry}>
+        <input
+          placeholder="enter city"
+          value={query}
+          onChange={handleChange}
+        ></input>
+      </form>
+    </>
   );
 }
 
